147fe9c31767956b2264cc1057f6d7b92f787634
VictorMiro <viktor.myroshnychenko@nure.ua>
2019-03-14 15:48:17 +0200

    some small changes

--- datingsite/settings.py
+++ datingsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'datingcore.apps.DatingcoreConfig',
-    'debug_toolbar',
+    # 'debug_toolbar',
     'messagesOF',
     'bootstrapform',
     'rest_framework'
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'datingsite.urls'
